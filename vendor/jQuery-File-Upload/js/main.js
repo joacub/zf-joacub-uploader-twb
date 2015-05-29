dbc87a194abf84a974f119bd3c95a27309648631
Johan Rodriguez Ramos <joacub@gmail.com>
2015-05-29 19:39:44 +0200

    cambios

--- vendor/jQuery-File-Upload/js/main.js
+++ vendor/jQuery-File-Upload/js/main.js
@@ -15,76 +15,76 @@
 $(function () {
     'use strict';
     $('.fileupload-handler').each(function() {
-        var $t = $(this);
+		var $t = $(this);
 
-        var options = {
-            destroy: function (e, data) {
-                data.dataType = 'json';
-                var that = $(this).data('blueimp-fileupload') ||
-                    $(this).data('fileupload');
-                if (data.url) {
-                    $.ajax(data).done(function (result) {
-
-                        if(!result.success) {
-                            $(this).find('.error-on-delete').html('<span class="label label-important">Error</span> ' + result.error);
-                            return false;
-                        }
-
-                        that._transition(data.context).done(
-                            function () {
-                                $(this).remove();
-                                that._trigger('destroyed', e, data);
-                            }
-                        );
-                    });
+    	var options = {
+    			destroy: function (e, data) {
+    				data.dataType = 'json';
+                    var that = $(this).data('blueimp-fileupload') ||
+                            $(this).data('fileupload');
+                    if (data.url) {
+                        $.ajax(data).done(function (result) {
+                        	
+                        	if(!result.success) {
+                        		$(this).find('.error-on-delete').html('<span class="label label-important">Error</span> ' + result.error);
+                        		return false;
+                        	}
+                        	
+                            that._transition(data.context).done(
+                                function () {
+                                    $(this).remove();
+                                    that._trigger('destroyed', e, data);
+                                }
+                            );
+                        });
+                    }
                 }
-            }
 
-        };
+    	};
 
-        $.extend(options, optionsZfJoacubUploaderTwb[$t.prop('id')]);
+    	$.extend(options, optionsZfJoacubUploaderTwb[$t.prop('id')]);
+    	
+	    // Initialize the jQuery File Upload widget:
+    	$t.fileupload(options);
 
-        // Initialize the jQuery File Upload widget:
-        $t.fileupload(options);
-
-        // Enable iframe cross-domain access via redirect option:
-        $t.fileupload(
-            'option',
-            'redirect',
-            window.location.href.replace(
-                /\/[^\/]*$/,
-                '/cors/result.html?%s'
-            )
-        );
-
-
-        if ($.support.cors) {
-            $.ajax({
-                url: $t.fileupload('option', 'url'),
-                type: 'HEAD'
-            }).fail(function () {
-                $('<div class="alert alert-danger"/>')
-                    .text('Subir al servidor no está disponible - ' +
-                    new Date())
-                    .appendTo($t);
-            });
-        }
-
+	    // Enable iframe cross-domain access via redirect option:
+    	$t.fileupload(
+	        'option',
+	        'redirect',
+	        window.location.href.replace(
+	            /\/[^\/]*$/,
+	            '/cors/result.html?%s'
+	        )
+	    );
+    	
+    	
+    	 if ($.support.cors) {
+	            $.ajax({
+	                url: $t.fileupload('option', 'url'),
+	                type: 'HEAD'
+	            }).fail(function () {
+	                $('<div class="alert alert-danger"/>')
+	                    .text('Subir al servidor no está disponible - ' +
+	                            new Date())
+	                    .appendTo($t);
+	            });
+	        }
+    	 
         // Load existing files:
-        $t.addClass('fileupload-processing');
-        $.ajax({
-            // Uncomment the following to send cross-domain cookies:
-            //xhrFields: {withCredentials: true},
-            url: $t.fileupload('option', 'url'),
-            dataType: 'json',
-            context: $t[0]
-        }).always(function () {
-            $(this).removeClass('fileupload-processing');
-        }).done(function (result) {
-            $(this).fileupload('option', 'done')
-                .call(this, null, {result: result});
-        });
-
+    	$t.addClass('fileupload-processing');
+    	 $.ajax({
+	            // Uncomment the following to send cross-domain cookies:
+	            //xhrFields: {withCredentials: true},
+	            url: $t.fileupload('option', 'url'),
+	            dataType: 'json',
+	            context: $t[0]
+	        }).always(function () {
+	            $(this).removeClass('fileupload-processing');
+	        }).done(function (result) {
+	            $(this).fileupload('option', 'done')
+	                .call(this, null, {result: result});
+	        });
+    	 
     });
 
 });
